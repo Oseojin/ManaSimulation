0a3a089cab750d657e5161d469534365aa763d77
Oseojin <77391201+Oseojin@users.noreply.github.com>
2023-09-25 18:40:37 +0900

    notebook Process Finish

--- 2DManaSimulation/Assets/Scripts/ManaPlacer.cs
+++ 2DManaSimulation/Assets/Scripts/ManaPlacer.cs
@@ -1,3 +1,4 @@
+using System.CodeDom.Compiler;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,11 +20,33 @@ public class ManaPlacer : MonoBehaviour
         int width = simulationManager.width;
         int height = simulationManager.height;
         simulationManager.manaMap = new float[width, height];
-        // For example, place initial mana at the center of the grid
+        PlaceCenter(width, height);
+    }
+
+    private void PlaceCenter(int width, int height)
+    {
+        simulationManager.manaMap[width / 2, height / 2] = initialMana;
+    }
+
+    private void PlaceVertex(int width, int height)
+    {
         simulationManager.manaMap[0, 0] = initialMana;
         simulationManager.manaMap[width - 1, 0] = initialMana;
         simulationManager.manaMap[0, height - 1] = initialMana;
-        simulationManager.manaMap[width - 1, height -1] = initialMana;
-        //simulationManager.manaMap[width / 2, height / 2] = initialMana;
+        simulationManager.manaMap[width - 1, height - 1] = initialMana;
+    }
+
+    private void PlaceBorderLine(int width, int height)
+    {
+        for(int i = 0; i < width; i++)
+        {
+            simulationManager.manaMap[i, 0] = initialMana;
+            simulationManager.manaMap[i, height - 1] = initialMana;
+        }
+        for(int i = 0; i < height; i++)
+        {
+            simulationManager.manaMap[0, i] = initialMana;
+            simulationManager.manaMap[width - 1, i] = initialMana;
+        }
     }
 }
